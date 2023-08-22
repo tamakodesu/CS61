192eef3ecd451b23cc08f898ef74133d0cbc96fb
tamakodesu <141847609+tamakodesu@users.noreply.github.com>
2023-08-22 13:36:15 +0800

    Add files via upload

--- lab03/src/BombMain.java
+++ lab03/src/BombMain.java
@@ -10,13 +10,19 @@ public class BombMain {
         //              take you to the method definition.
         Bomb b = new Bomb();
         if (phase >= 0) {
-            b.phase0(null); // Figure this out. I wonder where the phases are defined...
+            b.phase0("39291226"); // Figure this out. I wonder where the phases are defined...
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out next
+            int[] pass={0,9,3,0,8};
+            b.phase1(pass); // Figure this out next
         }
         if (phase >= 2) {
-            b.phase2("Figure this out last!");
+            String pass= "";
+            for(int i=0;i<1337;i++){
+                pass +="1 ";
+            }
+            pass += "-758328628";
+            b.phase2(pass);
         }
     }
 }
---
+++ lab03/tests/CounterTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+
+public class CounterTest {
+
+    @Test
+    public void increment() {
+        Counter c = new Counter();
+        c.increment();
+        assertWithMessage("Counter value is not 1.").that(c.value()).isEqualTo(1);
+        c.increment();
+        assertWithMessage("Counter value is not 2.").that(c.value()).isEqualTo(2);
+    }
+
+    @Test
+    public void reset() {
+        Counter c = new Counter();
+        c.increment();
+        c.reset();
+        assertWithMessage("Counter value is not 0 after reset.").that(c.value()).isEqualTo(0);
+    }
+
+    @Test
+    public void testConstructor() {
+        Counter c = new Counter();
+        assertWithMessage("Counter value is not 0 upon instantiation.").that(c.value()).isEqualTo(0);
+    }
+
+}
