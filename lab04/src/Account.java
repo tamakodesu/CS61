1c5e90c541e4313cb3b9dded4937d3192ca34436
tamakodesu <141847609+tamakodesu@users.noreply.github.com>
2023-08-26 16:58:17 +0800

    Add files via upload

--- lab04/src/Account.java
+++ lab04/src/Account.java
@@ -6,16 +6,21 @@ public class Account {
 
     private int balance;
 
+
     /** Initialize an account with the given balance. */
     public Account(int balance) {
         this.balance = balance;
+        this.parentAccount=null;
     }
-
+    Account parentAccount;
     /** Returns the balance for the current account. */
     public int getBalance() {
         return balance;
     }
-
+    public Account(int balance,Account parent){
+        this.balance=balance;
+        this.parentAccount=parent;
+    }
     /** Deposits amount into the current account. */
     public void deposit(int amount) {
         if (amount < 0) {
@@ -30,15 +35,22 @@ public class Account {
      * would leave a negative balance, print an error message and leave the
      * balance unchanged.
      */
-    public void withdraw(int amount) {
+    public boolean withdraw(int amount) {
         // TODO
         if (amount < 0) {
             System.out.println("Cannot withdraw negative amount.");
-        } else if (balance < amount) {
+        } else if ((this.parentAccount==null && this.balance < amount) ||(this.parentAccount!=null && this.getBalance()+this.parentAccount.getBalance() < amount)) {
             System.out.println("Insufficient funds");
         } else {
             balance -= amount;
+            if(balance<0)
+            {
+                this.parentAccount.balance+=this.balance;
+                this.balance=0;
+            }
+            return true;
         }
+        return false;
     }
 
     /**
@@ -46,6 +58,9 @@ public class Account {
      * and depositing it into this account.
      */
     public void merge(Account other) {
+        this.balance=this.getBalance()+other.getBalance();
+        other.balance=0;
         // TODO
     }
+
 }
--- lab04/src/Path.java
+++ lab04/src/Path.java
@@ -1,6 +1,29 @@
 /** A class that represents a path via pursuit curves. */
 public class Path {
-
+    private double x1,x2;
+    private double y1,y2;
+    public Path(double x,double y){
+        this.x1=x+0.0001;
+        this.y1=y+0.0001;
+    }
+    public double getCurrX(){
+        return this.x1;
+    }
+    public double getCurrY(){
+        return this.y1;
+    }
+    public double getNextX(){
+        return this.x2;
+    }
+    public double getNextY(){
+        return this.y2;
+    }
+    public void iterate(double dx,double dy){
+        this.x1=x2;
+        this.y1=y2;
+        this.x2+=dx;
+        this.y2+=dy;
+    }
     // TODO
 
 }
